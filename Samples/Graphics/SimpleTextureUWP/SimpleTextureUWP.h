20836c8f96f1fc626bd0595958e4cfa4e3abe4d4
Chuck Walbourn <chuckw@windows.microsoft.com>
2016-04-28 13:58:53 -0700

    Added SimpleTexture

---
+++ Samples/Graphics/SimpleTextureUWP/SimpleTextureUWP.h
@@ -0,0 +1,72 @@
+//--------------------------------------------------------------------------------------
+// SimpleTextureUWP.h
+//
+// Advanced Technology Group (ATG)
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "DeviceResources.h"
+#include "StepTimer.h"
+
+
+// A basic sample implementation that creates a D3D11 device and
+// provides a render loop.
+class Sample : public DX::IDeviceNotify
+{
+public:
+
+    Sample();
+
+    // Initialization and management
+    void Initialize(IUnknown* window, int width, int height, DXGI_MODE_ROTATION rotation);
+
+    // Basic render loop
+    void Tick();
+    void Render();
+
+    // Rendering helpers
+    void Clear();
+
+    // IDeviceNotify
+    virtual void OnDeviceLost() override;
+    virtual void OnDeviceRestored() override;
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowSizeChanged(int width, int height, DXGI_MODE_ROTATION rotation);
+    void ValidateDevice();
+
+    // Properties
+    void GetDefaultSize( int& width, int& height ) const;
+
+private:
+
+    void Update(DX::StepTimer const& timer);
+
+    void CreateDeviceDependentResources();
+    void CreateWindowSizeDependentResources();
+    
+    // Device resources.
+    std::unique_ptr<DX::DeviceResources>    m_deviceResources;
+
+    // Rendering loop timer.
+    DX::StepTimer                           m_timer;
+
+    // Input devices.
+    std::unique_ptr<DirectX::GamePad>       m_gamePad;
+    std::unique_ptr<DirectX::Keyboard>      m_keyboard;
+
+    // Sample objects
+    Microsoft::WRL::ComPtr<ID3D11InputLayout>           m_spInputLayout;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>                m_spVertexBuffer;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>                m_spIndexBuffer;
+    Microsoft::WRL::ComPtr<ID3D11VertexShader>          m_spVertexShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>           m_spPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11SamplerState>          m_spSampler;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_spTexture;
+};
---
+++ Samples/Graphics/SimpleTextureUWP12/SimpleTextureUWP12.h
@@ -0,0 +1,73 @@
+//--------------------------------------------------------------------------------------
+// SimpleTextureUWP12.h
+//
+// Advanced Technology Group (ATG)
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "DeviceResources.h"
+#include "StepTimer.h"
+
+
+// A basic sample implementation that creates a D3D12 device and
+// provides a render loop.
+class Sample : public DX::IDeviceNotify
+{
+public:
+
+    Sample();
+
+    // Initialization and management
+    void Initialize(IUnknown* window, int width, int height, DXGI_MODE_ROTATION rotation);
+
+    // Basic render loop
+    void Tick();
+    void Render();
+
+    // Rendering helpers
+    void Clear();
+
+    // IDeviceNotify
+    virtual void OnDeviceLost() override;
+    virtual void OnDeviceRestored() override;
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnWindowSizeChanged(int width, int height, DXGI_MODE_ROTATION rotation);
+    void ValidateDevice();
+
+    // Properties
+    void GetDefaultSize( int& width, int& height ) const;
+
+private:
+
+    void Update(DX::StepTimer const& timer);
+
+    void CreateDeviceDependentResources();
+    void CreateWindowSizeDependentResources();
+    
+    // Device resources.
+    std::unique_ptr<DX::DeviceResources>    m_deviceResources;
+
+    // Rendering loop timer.
+    DX::StepTimer                           m_timer;
+
+    // Input devices.
+    std::unique_ptr<DirectX::GamePad>       m_gamePad;
+    std::unique_ptr<DirectX::Keyboard>      m_keyboard;
+
+    // Direct3D 12 objects
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>    m_srvHeap;
+    Microsoft::WRL::ComPtr<ID3D12RootSignature>     m_rootSignature;
+    Microsoft::WRL::ComPtr<ID3D12PipelineState>     m_pipelineState;
+    Microsoft::WRL::ComPtr<ID3D12Resource>          m_vertexBuffer;
+    Microsoft::WRL::ComPtr<ID3D12Resource>          m_indexBuffer;
+    Microsoft::WRL::ComPtr<ID3D12Resource>          m_texture;
+    D3D12_VERTEX_BUFFER_VIEW                        m_vertexBufferView;
+    D3D12_INDEX_BUFFER_VIEW                         m_indexBufferView;
+};
