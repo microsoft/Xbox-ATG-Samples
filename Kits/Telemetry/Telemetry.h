a0f5210342d8eee96859e96f33b70b64cab8bda2
Chuck Walbourn <chuckw@windows.microsoft.com>
2016-12-08 21:42:09 -0800

    Added missing files

---
+++ Kits/Telemetry/Telemetry.h
@@ -0,0 +1,30 @@
+﻿//--------------------------------------------------------------------------------------
+// Telemetry.h
+//
+// Advanced Technology Group (ATG)
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+#pragma once
+
+#ifdef ATG_TELEMETRY_EXPORTS
+#define ATG_TELEMETRY_API __declspec(dllexport)
+#else
+#define ATG_TELEMETRY_API __declspec(dllimport)
+#endif
+
+#include <stdint.h>
+
+namespace ATG
+{
+    // Must register the event provider before writing any events
+    ATG_TELEMETRY_API uint32_t EventRegisterATGSampleTelemetry();
+    ATG_TELEMETRY_API uint32_t EventUnregisterATGSampleTelemetry();
+
+    // Returns true if you've called EventRegisterATGSampleTelemetry()
+    ATG_TELEMETRY_API bool EventEnabledSampleLoaded();
+
+    // Log the SampleLoaded event
+    // requires that you call EventRegisterATGSampleTelemetry() first
+    ATG_TELEMETRY_API uint32_t EventWriteSampleLoaded(const wchar_t* exeName);
+}
