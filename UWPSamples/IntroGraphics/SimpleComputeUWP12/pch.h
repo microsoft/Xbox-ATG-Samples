507ad0c9bd7188d84b11fb7f5bd0fe578af60d28
Chuck Walbourn <chuckw@windows.microsoft.com>
2017-09-29 12:05:52 -0700

    Added SimpleCompute samples for UWP

---
+++ UWPSamples/IntroGraphics/SimpleComputeUWP12/pch.h
@@ -0,0 +1,83 @@
+﻿//--------------------------------------------------------------------------------------
+// pch.h
+//
+// Header for standard system include files.
+//
+// Advanced Technology Group (ATG)
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+#pragma once
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+#pragma warning(push)
+#pragma warning(disable : 4467)
+#include <wrl.h>
+#pragma warning(pop)
+
+#include <d3d12.h>
+
+#if defined(NTDDI_WIN10_RS2)
+#include <dxgi1_6.h>
+#else
+#include <dxgi1_5.h>
+#endif
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#include "d3dx12.h"
+
+#include <algorithm>
+#include <atomic>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+#include <thread>
+
+#include <stdio.h>
+#include <pix.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include "GamePad.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "RenderTargetState.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+
+namespace DX
+{
+    // Helper class for COM exceptions
+    class com_exception : public std::exception
+    {
+    public:
+        com_exception(HRESULT hr) : result(hr) {}
+
+        virtual const char* what() const override
+        {
+            static char s_str[64] = {};
+            sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<unsigned int>(result));
+            return s_str;
+        }
+
+    private:
+        HRESULT result;
+    };
+
+    // Helper utility converts D3D API failures into exceptions.
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            throw com_exception(hr);
+        }
+    }
+}
