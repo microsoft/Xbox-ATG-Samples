632e6d68493aa64a702f6ba1091c8048438748ec
Chuck Walbourn <chuckw@windows.microsoft.com>
2018-11-02 19:25:41 -0700

    TextConsole updated to support DX12

--- Kits/ATGTK/TextConsole.cpp
+++ Kits/ATGTK/TextConsole.cpp
@@ -29,6 +29,23 @@ TextConsole::TextConsole()
 }
 
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+_Use_decl_annotations_
+TextConsole::TextConsole(
+    ID3D12Device* device,
+    ResourceUploadBatch& upload,
+    const RenderTargetState& rtState,
+    const wchar_t* fontName,
+    D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptor, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptor)
+    : m_foregroundColor(1.f, 1.f, 1.f, 1.f),
+    m_debugOutput(false)
+{
+    RestoreDevice(device, upload, rtState, fontName, cpuDescriptor, gpuDescriptor);
+
+    Clear();
+}
+#else
+_Use_decl_annotations_
 TextConsole::TextConsole(ID3D11DeviceContext* context, const wchar_t* fontName)
     : m_foregroundColor(1.f, 1.f, 1.f, 1.f),
     m_debugOutput(false)
@@ -37,9 +54,14 @@ TextConsole::TextConsole(ID3D11DeviceContext* context, const wchar_t* fontName)
 
     Clear();
 }
+#endif
 
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+void TextConsole::Render(_In_ ID3D12GraphicsCommandList* commandList)
+#else
 void TextConsole::Render()
+#endif
 {
     std::lock_guard<std::mutex> lock(m_mutex);
 
@@ -48,9 +70,13 @@ void TextConsole::Render()
     float x = float(m_layout.left);
     float y = float(m_layout.top);
     
-	XMVECTOR foregroundColor = XMLoadFloat4(&m_foregroundColor);
+    XMVECTOR foregroundColor = XMLoadFloat4(&m_foregroundColor);
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+    m_batch->Begin(commandList);
+#else
     m_batch->Begin();
+#endif
 
     unsigned int textLine = unsigned int(m_currentLine + 1) % m_rows;
     
@@ -60,8 +86,8 @@ void TextConsole::Render()
 
         if (*(m_lines[textLine].m_text))
         {
-			XMVECTOR lineColor = XMLoadFloat4(&m_lines[textLine].m_textColor);
-			m_font->DrawString(m_batch.get(), m_lines[textLine].m_text, pos, XMColorEqual(lineColor, Line::s_defaultColor) ? foregroundColor : lineColor);
+            XMVECTOR lineColor = XMLoadFloat4(&m_lines[textLine].m_textColor);
+            m_font->DrawString(m_batch.get(), m_lines[textLine].m_text, pos, XMColorEqual(lineColor, Line::s_defaultColor) ? foregroundColor : lineColor);
         }
 
         textLine = unsigned int(textLine + 1) % m_rows;
@@ -80,37 +106,37 @@ void TextConsole::Clear()
         memset(m_buffer.get(), 0, sizeof(wchar_t) * (m_columns + 1) * m_rows);
     }
 
-	if (m_lines)
-	{
-		for (unsigned int line = 0; line < m_rows; ++line)
-		{
-			m_lines[line].SetColor();
-		}
-	}
-	
-	m_currentColumn = m_currentLine = 0;
+    if (m_lines)
+    {
+        for (unsigned int line = 0; line < m_rows; ++line)
+        {
+            m_lines[line].SetColor();
+        }
+    }
+    
+    m_currentColumn = m_currentLine = 0;
 }
 
 
 _Use_decl_annotations_
 void TextConsole::Write(const wchar_t* str)
 {
-	Write(Line::s_defaultColor, str);
+    Write(Line::s_defaultColor, str);
 }
 
 
 _Use_decl_annotations_
 void XM_CALLCONV TextConsole::Write(FXMVECTOR color, const wchar_t* str)
 {
-	std::lock_guard<std::mutex> lock(m_mutex);
+    std::lock_guard<std::mutex> lock(m_mutex);
 
-	ProcessString(color, str);
+    ProcessString(color, str);
 
 #ifndef NDEBUG
-	if (m_debugOutput)
-	{
-		OutputDebugStringW(str);
-	}
+    if (m_debugOutput)
+    {
+        OutputDebugStringW(str);
+    }
 #endif
 }
 
@@ -118,24 +144,24 @@ void XM_CALLCONV TextConsole::Write(FXMVECTOR color, const wchar_t* str)
 _Use_decl_annotations_
 void TextConsole::WriteLine(const wchar_t* str)
 {
-	WriteLine(Line::s_defaultColor, str);
+    WriteLine(Line::s_defaultColor, str);
 }
 
 
 _Use_decl_annotations_
 void XM_CALLCONV TextConsole::WriteLine(FXMVECTOR color, const wchar_t* str)
 {
-	std::lock_guard<std::mutex> lock(m_mutex);
+    std::lock_guard<std::mutex> lock(m_mutex);
 
-	ProcessString(color, str);
-	IncrementLine();
+    ProcessString(color, str);
+    IncrementLine();
 
 #ifndef NDEBUG
-	if (m_debugOutput)
-	{
-		OutputDebugStringW(str);
-		OutputDebugStringW(L"\n");
-	}
+    if (m_debugOutput)
+    {
+        OutputDebugStringW(str);
+        OutputDebugStringW(L"\n");
+    }
 #endif
 }
 
@@ -143,48 +169,48 @@ void XM_CALLCONV TextConsole::WriteLine(FXMVECTOR color, const wchar_t* str)
 _Use_decl_annotations_
 void TextConsole::Format(const wchar_t* strFormat, ...)
 {
-	va_list argList;
-	va_start(argList, strFormat);
+    va_list argList;
+    va_start(argList, strFormat);
 
-	FormatImpl(Line::s_defaultColor, strFormat, argList);
+    FormatImpl(Line::s_defaultColor, strFormat, argList);
 
-	va_end(argList);
+    va_end(argList);
 }
 
 
 _Use_decl_annotations_
 void XM_CALLCONV TextConsole::Format(CXMVECTOR color, const wchar_t* strFormat, ...)
 {
-	va_list argList;
-	va_start(argList, strFormat);
+    va_list argList;
+    va_start(argList, strFormat);
 
-	FormatImpl(color, strFormat, argList);
+    FormatImpl(color, strFormat, argList);
 
-	va_end(argList);
+    va_end(argList);
 }
 
 
 _Use_decl_annotations_
 void XM_CALLCONV TextConsole::FormatImpl(FXMVECTOR color, const wchar_t* strFormat, va_list args)
 {
-	std::lock_guard<std::mutex> lock(m_mutex);
+    std::lock_guard<std::mutex> lock(m_mutex);
 
-	size_t len = _vscwprintf(strFormat, args) + 1;
+    size_t len = _vscwprintf(strFormat, args) + 1;
 
-	if (m_tempBuffer.size() < len)
-		m_tempBuffer.resize(len);
+    if (m_tempBuffer.size() < len)
+        m_tempBuffer.resize(len);
 
-	memset(m_tempBuffer.data(), 0, sizeof(wchar_t) * len);
+    memset(m_tempBuffer.data(), 0, sizeof(wchar_t) * len);
 
-	vswprintf_s(m_tempBuffer.data(), m_tempBuffer.size(), strFormat, args);
+    vswprintf_s(m_tempBuffer.data(), m_tempBuffer.size(), strFormat, args);
 
-	ProcessString(color, m_tempBuffer.data());
+    ProcessString(color, m_tempBuffer.data());
 
 #ifndef NDEBUG
-	if (m_debugOutput)
-	{
-		OutputDebugStringW(m_tempBuffer.data());
-	}
+    if (m_debugOutput)
+    {
+        OutputDebugStringW(m_tempBuffer.data());
+    }
 #endif
 }
 
@@ -220,7 +246,7 @@ void TextConsole::SetWindow(const RECT& layout)
         for (unsigned int line = 0; line < r; ++line)
         {
             memcpy(lines[line].m_text, m_lines[line].m_text, c * sizeof(wchar_t));
-			lines[line].m_textColor = m_lines[line].m_textColor;
+            lines[line].m_textColor = m_lines[line].m_textColor;
         }
     }
 
@@ -240,10 +266,41 @@ void TextConsole::ReleaseDevice()
 {
     m_batch.reset();
     m_font.reset();
+#if defined(__d3d11_h__) || defined(__d3d11_x_h__)
     m_context.Reset();
+#endif
 }
 
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+_Use_decl_annotations_
+void TextConsole::RestoreDevice(
+    ID3D12Device* device,
+    ResourceUploadBatch& upload,
+    const RenderTargetState& rtState,
+    const wchar_t* fontName,
+    D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptor, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptor)
+
+{
+    {
+        SpriteBatchPipelineStateDescription pd(rtState);
+        m_batch = std::make_unique<SpriteBatch>(device, upload, pd);
+    }
+
+    m_font = std::make_unique<SpriteFont>(device, upload, fontName, cpuDescriptor, gpuDescriptor);
+
+    m_font->SetDefaultCharacter(L' ');
+}
+
+
+void TextConsole::SetViewport(const D3D12_VIEWPORT& viewPort)
+{
+    if (m_batch)
+    {
+        m_batch->SetViewport(viewPort);
+    }
+}
+#else
 void TextConsole::RestoreDevice(ID3D11DeviceContext* context, const wchar_t* fontName)
 {
     m_context = context;
@@ -266,6 +323,7 @@ void TextConsole::SetViewport(const D3D11_VIEWPORT& viewPort)
         m_batch->SetViewport(viewPort);
     }
 }
+#endif
 
 
 void TextConsole::SetRotation(DXGI_MODE_ROTATION rotation)
@@ -277,12 +335,13 @@ void TextConsole::SetRotation(DXGI_MODE_ROTATION rotation)
 }
 
 
+_Use_decl_annotations_
 void TextConsole::ProcessString(FXMVECTOR color, const wchar_t* str)
 {
     if (!m_lines)
         return;
 
-	m_lines[m_currentLine].SetColor(color);
+    m_lines[m_currentLine].SetColor(color);
 
     float width = float(m_layout.right - m_layout.left);
 
@@ -291,8 +350,8 @@ void TextConsole::ProcessString(FXMVECTOR color, const wchar_t* str)
         if (*ch == '\n')
         {
             IncrementLine();
-			m_lines[m_currentLine].SetColor(color);
-			continue;
+            m_lines[m_currentLine].SetColor(color);
+            continue;
         }
 
         bool increment = false;
@@ -318,8 +377,8 @@ void TextConsole::ProcessString(FXMVECTOR color, const wchar_t* str)
         {
             IncrementLine();
             m_lines[m_currentLine].m_text[0] = *ch;
-			m_lines[m_currentLine].SetColor(color);
-		}
+            m_lines[m_currentLine].SetColor(color);
+        }
 
         ++m_currentColumn;
     }
@@ -338,19 +397,61 @@ void TextConsole::IncrementLine()
 
 
 //--------------------------------------------------------------------------------------
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+TextConsoleImage::TextConsoleImage() :
+    TextConsole(),
+    m_bgGpuDescriptor{},
+    m_bgSize{}
+{
+}
+#else
 TextConsoleImage::TextConsoleImage() :
     TextConsole()
 {
 }
+#endif
 
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+_Use_decl_annotations_
+TextConsoleImage::TextConsoleImage(
+    ID3D12Device* device,
+    ResourceUploadBatch& upload,
+    const RenderTargetState& rtState,
+    const wchar_t* fontName,
+    const wchar_t* image,
+    D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorFont, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorFont,
+    D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorImage, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorImage) :
+    TextConsole(),
+    m_bgGpuDescriptor{},
+    m_bgSize{}
+{
+    RestoreDevice(device, upload, rtState, fontName, image,
+        cpuDescriptorFont, gpuDescriptorFont,
+        cpuDescriptorImage, gpuDescriptorImage);
+}
+#else
+_Use_decl_annotations_
 TextConsoleImage::TextConsoleImage(ID3D11DeviceContext* context, const wchar_t* fontName, const wchar_t* image) :
     TextConsole()
 {
     RestoreDevice(context, fontName, image);
 }
+#endif
 
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+void TextConsoleImage::Render(_In_ ID3D12GraphicsCommandList* commandList)
+{
+    m_batch->Begin(commandList);
+
+    m_batch->Draw(m_bgGpuDescriptor, m_bgSize, m_fullscreen);
+
+    m_batch->End();
+
+    TextConsole::Render(commandList);
+}
+#else
 void TextConsoleImage::Render()
 {
     m_batch->Begin();
@@ -361,6 +462,7 @@ void TextConsoleImage::Render()
 
     TextConsole::Render();
 }
+#endif
 
 
 void TextConsoleImage::SetWindow(const RECT& fullscreen, bool useSafeRect)
@@ -381,8 +483,14 @@ void TextConsoleImage::SetWindow(const RECT& fullscreen, bool useSafeRect)
     UINT width = std::max<UINT>(fullscreen.right - fullscreen.left, 1);
     UINT height = std::max<UINT>(fullscreen.bottom - fullscreen.top, 1);
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+    D3D12_VIEWPORT vp = { 0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height),
+        D3D12_DEFAULT_VIEWPORT_MIN_DEPTH, D3D12_DEFAULT_VIEWPORT_MAX_DEPTH };
+    m_batch->SetViewport(vp);
+#else
     auto vp = CD3D11_VIEWPORT(0.0f, 0.0f, static_cast<float>(width), static_cast<float>(height));
     m_batch->SetViewport(vp);
+#endif
 }
 
 
@@ -394,6 +502,50 @@ void TextConsoleImage::ReleaseDevice()
 }
 
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+_Use_decl_annotations_
+void TextConsoleImage::RestoreDevice(
+    ID3D12Device* device,
+    DirectX::ResourceUploadBatch& upload,
+    const DirectX::RenderTargetState& rtState,
+    const wchar_t* fontName,
+    const wchar_t* image,
+    D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorFont, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorFont,
+    D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorImage, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorImage)
+{
+    TextConsole::RestoreDevice(device, upload, rtState, fontName, cpuDescriptorFont, gpuDescriptorFont);
+
+    wchar_t ext[_MAX_EXT];
+    _wsplitpath_s(image, nullptr, 0, nullptr, 0, nullptr, 0, ext, _MAX_EXT);
+
+    if (_wcsicmp(ext, L".dds") == 0)
+    {
+        DX::ThrowIfFailed(CreateDDSTextureFromFile(device, upload, image, m_background.ReleaseAndGetAddressOf()));
+    }
+    else
+    {
+        DX::ThrowIfFailed(CreateWICTextureFromFile(device, upload, image, m_background.ReleaseAndGetAddressOf()));
+    }
+
+    auto desc = m_background->GetDesc();
+    if (desc.Dimension != D3D12_RESOURCE_DIMENSION_TEXTURE2D)
+    {
+        throw std::exception("Only supports 2D images");
+    }
+
+    D3D12_SHADER_RESOURCE_VIEW_DESC SRVDesc = {};
+    SRVDesc.Format = desc.Format;
+    SRVDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+    SRVDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+    SRVDesc.Texture2D.MipLevels = (!desc.MipLevels) ? -1 : desc.MipLevels;
+    
+    device->CreateShaderResourceView(m_background.Get(), &SRVDesc, cpuDescriptorImage);
+
+    m_bgGpuDescriptor = gpuDescriptorImage;
+    m_bgSize = XMUINT2(static_cast<uint32_t>(desc.Width), desc.Height);
+}
+#else
+_Use_decl_annotations_
 void TextConsoleImage::RestoreDevice(ID3D11DeviceContext* context, const wchar_t* fontName, const wchar_t* image)
 {
     TextConsole::RestoreDevice(context, fontName);
@@ -413,3 +565,4 @@ void TextConsoleImage::RestoreDevice(ID3D11DeviceContext* context, const wchar_t
         DX::ThrowIfFailed(CreateWICTextureFromFile(device.Get(), image, nullptr, m_background.ReleaseAndGetAddressOf()));
     }
 }
+#endif
--- Kits/ATGTK/TextConsole.h
+++ Kits/ATGTK/TextConsole.h
@@ -25,7 +25,18 @@ namespace DX
     {
     public:
         TextConsole();
-        TextConsole(ID3D11DeviceContext* context, const wchar_t* fontName);
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+        TextConsole(
+            _In_ ID3D12Device* device,
+            DirectX::ResourceUploadBatch& upload,
+            const DirectX::RenderTargetState& rtState,
+            _In_z_ const wchar_t* fontName,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptor, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptor);
+#elif defined(__d3d11_h__) || defined(__d3d11_x_h__)
+        TextConsole(_In_ ID3D11DeviceContext* context, _In_z_ const wchar_t* fontName);
+#else
+#   error Please #include <d3d11.h> or <d3d12.h>
+#endif
 
         TextConsole(TextConsole&&) = default;
         TextConsole& operator= (TextConsole&&) = default;
@@ -33,18 +44,22 @@ namespace DX
         TextConsole(TextConsole const&) = delete;
         TextConsole& operator= (TextConsole const&) = delete;
 
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+        void Render(_In_ ID3D12GraphicsCommandList* commandList);
+#else
         void Render();
+#endif
 
         void Clear();
 
         void Write(_In_z_ const wchar_t* str);
-		void XM_CALLCONV Write(DirectX::FXMVECTOR color, _In_z_ const wchar_t* str);
+        void XM_CALLCONV Write(DirectX::FXMVECTOR color, _In_z_ const wchar_t* str);
 
         void WriteLine(_In_z_ const wchar_t* str);
-		void XM_CALLCONV WriteLine(DirectX::FXMVECTOR color, _In_z_ const wchar_t* str);
+        void XM_CALLCONV WriteLine(DirectX::FXMVECTOR color, _In_z_ const wchar_t* str);
 
-		void Format(_In_z_ _Printf_format_string_ const wchar_t* strFormat, ...);
-		void XM_CALLCONV Format(DirectX::CXMVECTOR color, _In_z_ _Printf_format_string_ const wchar_t* strFormat, ...);
+        void Format(_In_z_ _Printf_format_string_ const wchar_t* strFormat, ...);
+        void XM_CALLCONV Format(DirectX::CXMVECTOR color, _In_z_ _Printf_format_string_ const wchar_t* strFormat, ...);
 
         void SetWindow(const RECT& layout);
 
@@ -53,30 +68,41 @@ namespace DX
         void SetDebugOutput(bool debug) { m_debugOutput = debug; }
 
         void ReleaseDevice();
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+        void RestoreDevice(
+            _In_ ID3D12Device* device,
+            DirectX::ResourceUploadBatch& upload,
+            const DirectX::RenderTargetState& rtState,
+            _In_z_ const wchar_t* fontName,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptor, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptor);
+
+        void SetViewport(const D3D12_VIEWPORT& viewPort);
+#else
         void RestoreDevice(ID3D11DeviceContext* context, const wchar_t* fontName);
 
         void SetViewport(const D3D11_VIEWPORT& viewPort);
+#endif
 
         void SetRotation(DXGI_MODE_ROTATION rotation);
 
     protected:
-		void XM_CALLCONV FormatImpl(DirectX::FXMVECTOR color, _In_z_ _Printf_format_string_ const wchar_t* strFormat, va_list args);
-        void XM_CALLCONV ProcessString(DirectX::FXMVECTOR color, const wchar_t* str);
+        void XM_CALLCONV FormatImpl(DirectX::FXMVECTOR color, _In_z_ _Printf_format_string_ const wchar_t* strFormat, va_list args);
+        void XM_CALLCONV ProcessString(DirectX::FXMVECTOR color, _In_z_ const wchar_t* str);
         void IncrementLine();
 
-		struct Line
-		{
-			wchar_t*			m_text;
-			DirectX::XMFLOAT4	m_textColor;
+        struct Line
+        {
+            wchar_t*			m_text;
+            DirectX::XMFLOAT4	m_textColor;
 
-			static const DirectX::XMVECTORF32 s_defaultColor;
+            static const DirectX::XMVECTORF32 s_defaultColor;
 
-			Line() : m_text(nullptr), m_textColor(s_defaultColor) {}
+            Line() : m_text(nullptr), m_textColor(s_defaultColor) {}
 
-			void XM_CALLCONV SetColor(DirectX::FXMVECTOR color = s_defaultColor) { XMStoreFloat4(&m_textColor, color); }
-		};
+            void XM_CALLCONV SetColor(DirectX::FXMVECTOR color = s_defaultColor) { XMStoreFloat4(&m_textColor, color); }
+        };
 
-		RECT                                            m_layout;
+        RECT                                            m_layout;
         DirectX::XMFLOAT4                               m_foregroundColor;
 
         bool                                            m_debugOutput;
@@ -92,7 +118,9 @@ namespace DX
 
         std::unique_ptr<DirectX::SpriteBatch>           m_batch;
         std::unique_ptr<DirectX::SpriteFont>            m_font;
+#if defined(__d3d11_h__) || defined(__d3d11_x_h__)
         Microsoft::WRL::ComPtr<ID3D11DeviceContext>     m_context;
+#endif
 
         std::mutex                                      m_mutex;
     };
@@ -101,7 +129,18 @@ namespace DX
     {
     public:
         TextConsoleImage();
-        TextConsoleImage(ID3D11DeviceContext* context, const wchar_t* fontName, const wchar_t* image);
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+        TextConsoleImage(
+            _In_ ID3D12Device* device,
+            DirectX::ResourceUploadBatch& upload,
+            const DirectX::RenderTargetState& rtState,
+            _In_z_ const wchar_t* fontName,
+            _In_z_ const wchar_t* image,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorFont, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorFont,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorImage, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorImage);
+#else
+        TextConsoleImage(_In_ ID3D11DeviceContext* context, _In_z_ const wchar_t* fontName, _In_z_ const wchar_t* image);
+#endif
 
         TextConsoleImage(TextConsoleImage&&) = default;
         TextConsoleImage& operator= (TextConsoleImage&&) = default;
@@ -109,18 +148,44 @@ namespace DX
         TextConsoleImage(TextConsoleImage const&) = delete;
         TextConsoleImage& operator= (TextConsoleImage const&) = delete;
 
-
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+        void Render(_In_ ID3D12GraphicsCommandList* commandList);
+#else
         void Render();
+#endif
 
         void SetWindow(const RECT& layout) = delete;
         void SetWindow(const RECT& fullscreen, bool useSafeRect);
 
         void ReleaseDevice();
-        void RestoreDevice(ID3D11DeviceContext* context, const wchar_t* fontName) = delete;
-        void RestoreDevice(ID3D11DeviceContext* context, const wchar_t* fontName, const wchar_t* image);
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+        void RestoreDevice(
+            _In_ ID3D12Device* device,
+            DirectX::ResourceUploadBatch& upload,
+            const DirectX::RenderTargetState& rtState,
+            _In_z_ const wchar_t* fontName,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptor, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptor) = delete;
+        void RestoreDevice(
+            _In_ ID3D12Device* device,
+            DirectX::ResourceUploadBatch& upload,
+            const DirectX::RenderTargetState& rtState,
+            _In_z_ const wchar_t* fontName,
+            _In_z_ const wchar_t* image,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorFont, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorFont,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpuDescriptorImage, D3D12_GPU_DESCRIPTOR_HANDLE gpuDescriptorImage);
+#else
+        void RestoreDevice(_In_ ID3D11DeviceContext* context, _In_z_ const wchar_t* fontName) = delete;
+        void RestoreDevice(_In_ ID3D11DeviceContext* context, _In_z_ const wchar_t* fontName, _In_z_ const wchar_t* image);
+#endif
 
     private:
+#if defined(__d3d12_h__) || defined(__d3d12_x_h__)
+        D3D12_GPU_DESCRIPTOR_HANDLE                         m_bgGpuDescriptor;
+        DirectX::XMUINT2                                    m_bgSize;
+        Microsoft::WRL::ComPtr<ID3D12Resource>              m_background;
+#else
         Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>    m_background;
+#endif
         RECT                                                m_fullscreen;
     };
 }
